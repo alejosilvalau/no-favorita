9d175e4244c80fbd0e7caa95d166f39db30aa8d2
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-10-03 08:20:11 -0300

    Solucionado problema de scroll horizontal

---
+++ includes/header.php
@@ -0,0 +1,41 @@
+<?php
+include("../includes/conexion.inc");
+include("../includes/sesiones.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>No Favorita</title>
+  <link rel="icon" type="image/x-icon" href="../assets/favicon.png">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    rel="stylesheet">
+  <link href="https://db.onlinewebfonts.com/c/3591a95e9abd510d170b0ba4077e70d3?family=Nimbus+Sans+TW01Con" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css"
+    rel="stylesheet">
+  <link rel="stylesheet" href="../css/navbar.css">
+  <link rel="stylesheet" href="../css/footer.css">
+
+  <?php
+  // Incluir hojas de estilo específicas para cada página
+  if ($page == 'home') {
+    echo '<link rel="stylesheet" href="../css/home.css">';
+  } elseif ($page == 'locales' || $page == 'baja_descuento' || $page == 'promocion_local' || $page == 'promociones' || $page == 'reportes' || $page == 'historial') {
+    echo '<link rel="stylesheet" href="../css/listados.css">';
+  } elseif ($page == 'promociones') {
+    echo '<link rel="stylesheet" href="../css/promociones.css">';
+  } elseif ($page == 'alta_local' || $page == 'alta_novedades' || $page == 'eliminacion_local' || $page == 'eliminacion_novedades' || $page == 'gestionar_descuentos' || $page == 'modificacion_local' || $page == 'modificacion_novedades' || $page == 'seccion_administrador' || $page == 'validar_duenio' || $page == 'alta_descuento' || $page == 'gestion_promocion' || $page == 'gestionar_solicitud' || $page == 'modificacion_perfil') {
+    echo '<link rel="stylesheet" href="../css/admin_locales.css">';
+  } ?>
+
+</head>
+
+
+<body>
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
