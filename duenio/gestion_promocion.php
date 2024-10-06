4bb246493430d710ae30908f7aa753f823f33732
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-10-06 14:14:19 -0300

    Funcionalidad terminada

--- duenio/gestion_promocion.php
+++ duenio/gestion_promocion.php
@@ -1,6 +1,6 @@
 <?php
 $page = 'gestion_promocion';
-include ("../includes/navbar.php");
+include("../includes/navbar.php");
 
 if ($_SESSION['tipoUsuario'] !== 'Dueño de local') {
   header("Location: ../public/home.php"); // Redirigir si no es dueño de local
@@ -8,33 +8,33 @@ if ($_SESSION['tipoUsuario'] !== 'Dueño de local') {
 }
 ?>
 
-<div class="container">
-  <h2 class="mt-5 text-center">Gestión de Promociones</h2>
-  <div class="row mt-5">
+<div class="container ">
+  <h2 class="mt-5 text-center">GESTIÓN DE PROMOCIONES</h2>
+  <div class="row mt-4">
     <div class="col-md-4">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Dar de alta una promocion</h5>
+          <h5 class="card-title">ALTA PROMOCIÓN</h5>
           <p class="card-text">Agrega una nueva promoción al sistema.</p>
-          <a href="./alta_descuento.php" class="btn btn-primary btn-block">Generar Promoción</a>
+          <a href="./alta_descuento.php" class="btn btn-primary btn-block submit">Generar Promoción</a>
         </div>
       </div>
     </div>
     <div class="col-md-4">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Dar de baja una promoción</h5>
+          <h5 class="card-title">BAJA PROMOCIÓN</h5>
           <p class="card-text">Elimina una promoción del sistema.</p>
-          <a href="./baja_descuento.php" class="btn btn-primary btn-block">Eliminar Promoción</a>
+          <a href="./baja_descuento.php" class="btn btn-primary btn-block submit">Eliminar Promoción</a>
         </div>
       </div>
     </div>
     <div class="col-md-4">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Gestión de Solicitudes</h5>
-          <p class="card-text">Gestionar solicitudes de descuentos de los clientes.</p>
-          <a href="./gestionar_solicitud.php" class="btn btn-primary btn-block">Gestionar
+          <h5 class="card-title">GESTIÓN SOLICITUD</h5>
+          <p class="card-text">Gestionar solicitudes de descuentos.</p>
+          <a href="./gestionar_solicitud.php" class="btn btn-primary btn-block submit">Gestionar
             Solicitudes</a>
         </div>
       </div>
@@ -44,5 +44,5 @@ if ($_SESSION['tipoUsuario'] !== 'Dueño de local') {
 </div>
 
 <?php
-include ("../includes/footer.php");
+include("../includes/footer.php");
 ?>
