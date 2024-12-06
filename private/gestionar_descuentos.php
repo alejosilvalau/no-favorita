778f32e629740794a8d3dddfe24fd708f9a30659
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-12-06 12:14:33 -0300

    Eliminadas funciones de muestra de errores en varios archivos

--- private/gestionar_descuentos.php
+++ private/gestionar_descuentos.php
@@ -1,9 +1,7 @@
 <?php
 include_once('../includes/funciones_helpers.php');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 ob_start();
 $page = 'gestionar_descuentos';
 include("../includes/navbar.php");
--- public/confirmar.php
+++ public/confirmar.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 include ('../includes/sesiones.php');
 include ('../includes/conexion.inc'); // Archivo donde están tus credenciales de conexión a la base de datos
 
